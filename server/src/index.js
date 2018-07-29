85525fa1c84665fc8ac52470e19265b2d7fc27fc
manticarodrigo <manticarodrigo@gmail.com>
2018-07-29 12:59:14 -0400

    added server ping interval

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const resolvers = require('./resolvers')
+var http = require('http')
+
+setInterval(function() {
+    http.get('https://pool-game-tracker.herokuapp.com/')
+    http.get('https://pool-game-tracker-graphql.herokuapp.com/heroku/demo')
+}, 150000) // every 5 minutes (300000)
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -10,8 +16,8 @@ const server = new GraphQLServer({
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
       endpoint: process.env.PRISMA_ENDPOINT, // the endpoint of the Prisma API (value set in `.env`)
-      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
       // secret: process.env.PRISMA_SECRET, // only needed if specified in `database/prisma.yml` (value set in `.env`)
+      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
     }),
   }),
 })
