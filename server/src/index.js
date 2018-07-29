6351ce2885a32a55ec5eb9da4d7a9fe1f927236d
manticarodrigo <manticarodrigo@gmail.com>
2018-07-29 13:10:03 -0400

    https library for ping req

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,12 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const resolvers = require('./resolvers')
-var http = require('http')
+var https = require('https')
 
 setInterval(function() {
     console.log('ping')
-    http.get('https://pool-game-tracker.herokuapp.com/')
-    http.get('https://pool-game-tracker-graphql.herokuapp.com/heroku/demo')
+    https.get('https://pool-game-tracker.herokuapp.com/')
+    https.get('https://pool-game-tracker-graphql.herokuapp.com/heroku/demo')
 }, 150000) // every 5 minutes (300000)
 
 const server = new GraphQLServer({
